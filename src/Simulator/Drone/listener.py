fdac2c1ba26658c45c65a3f7d0e098c3363f3063
pricejiang <pricejmh0911@gmail.com>
2019-06-25 13:09:17 -0500

    modify listen py

--- src/Simulator/Drone/listener.py
+++ src/Simulator/Drone/listener.py
@@ -2,43 +2,31 @@ import rospy
 from geometry_msgs.msg import Point, Twist
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
+from hector_uav_msgs.srv import EnableMotors
 from math import sqrt
 
-
-class Listener:
-    def __init__(self, num):
-        rospy.init_node("Listener Drone", anonymous=True)
-        # Drone's position and orientation information
+class Drone:
+    def __init__(self, number):
+        # Drone's position and orientation inforamtion
         self._x = 0.0
         self._y = 0.0
         self._z = 0.0
         self._theta = 0.0
         self.goal = Point()
-        self.goal_received = False
-
-        # What to do if shut down
-        rospy.on_shutdown(self.shutdown)
 
         # Set up subscriber and publisher
-        my_number = "/drone" + str(num)
-        self.pos = rospy.Subscriber(my_number + "/ground_truth/state", Odometry, self.newPos)
-
-        rospy.loginfo("Subscribe to move!")
-        self.listen = rospy.Subscriber(my_number + "/receive_cmd", Twist, self.set_throttle)
-
+        my_number = "/drone" + str(number)
+        self.sub = rospy.Subscriber(my_number + "/ground_truth/state", Odometry, self.newPos)
         self.pub = rospy.Publisher(my_number + "/cmd_vel", Twist, queue_size=10)
 
-        rospy.spin()
-
-    def set_throttle(self, move_cmd):
-        # if not self.goal_sent:
-        self.goal_received = True
-        rospy.loginfo("Send goal!")
-        # pub_goal = rospy.Publisher("/car_goal/goal", PointStamped, queue_size=1)
-        # goal = PointStamped()
-        rospy.loginfo("Command received!")
-
-        self.pub.publish(move_cmd)
+        # Enable motors using ROS service
+        rospy.wait_for_service(my_number + '/enable_motors')
+        try:
+            # Set 'enable_motors' to be True
+            enable = rospy.ServiceProxy(my_number + '/enable_motors', EnableMotors)
+            resp = enable(True)
+        except rospy.ServiceException as e:
+            rospy.loginfo("Service call failed with %s", e)
 
     def newPos(self, msg):
         self._x = msg.pose.pose.position.x
@@ -48,9 +36,38 @@ class Listener:
         quat = msg.pose.pose.orientation
         (_, _, self._theta) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
 
+
+class Listener:
+    def __init__(self, num):
+        rospy.init_node("Listener Drone", anonymous=True)
+        self.numberOfDrones = num
+        self.drones = []
+        self.complete = []
+        self.success = 0
+        self.goal_received = False
+
+        # Setup drones
+        # for i in range(self.numberOfDrones):
+        #     self.drones.append(Drone(i + 1))
+        #     self.complete.append(0)
+        self.drone = Drone(1)
+        self.listen = rospy.Subscriber("/drone1/drive_cmd", Twist, self.set_throttle)
+        # What to do if shut down
+        rospy.on_shutdown(self.shutdown)
+        rospy.spin()
+
+    def set_throttle(self, move_cmd):
+        # if not self.goal_sent:
+        self.goal_received = True
+        # pub_goal = rospy.Publisher("/car_goal/goal", PointStamped, queue_size=1)
+        # goal = PointStamped()
+        rospy.loginfo("Command received!")
+
+        self.drone.pub.publish(move_cmd)
+
     def shutdown(self):
         rospy.loginfo("Stop Car")
-        self.pub.publish(0)
+        self.drone.pub.publish(0)
         # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
 
