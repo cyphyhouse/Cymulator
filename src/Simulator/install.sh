832ceffd9de582066b33f0bd3830239154dca8bc
zitongc2 <zitongc2@illinois.edu>
2019-06-27 16:28:44 -0500

    file submission

--- src/Simulator/install.sh
+++ src/Simulator/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~
-sudo apt install python3-pip
+sudo apt install -y  python3-pip
 
 if [ ! "$(rosversion -d)" ]; then
     # Setup your sources.list
@@ -14,7 +14,7 @@ if [ ! "$(rosversion -d)" ]; then
     ## First, make sure your Debian package index is up-to-date:
     sudo apt-get update
     ## Install ROS Kinetic Desktop Full: ROS, rqt, rviz, robot-generic libraries, 2D/3D simulators, navigation and 2D/3D perception
-    sudo apt-get install ros-kinetic-desktop-full
+    sudo apt-get -y install ros-kinetic-desktop-full
 
     # Initialize rosdep
     sudo rosdep init
@@ -32,6 +32,19 @@ if [ ! -x "$(command -v gazebo)" ]; then
     source ~/.bashrc
 fi
 
+pip3 install catkin_pkg
+pip3 install numpy
+pip3 install empy
+pip3 install pathlib
+sudo apt-get install -y ros-kinetic-geographic-msgs
+sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers
+sudo apt-get install -y ros-kinetic-ackermann-msgs
+sudo apt-get install -y python3-yaml
+sudo apt-get install -y python3-catkin-pkg-modules
+sudo apt-get install -y python3-rospkg-modules
+sudo apt-get install -y ros-kinetic-tf2-bullet
+
+#-------------------------------------------------------------------------
 # Download the catkin workspace
 cd ~
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qbtppHGp315hBXq-yoChIv1rNSiRz8_M' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qbtppHGp315hBXq-yoChIv1rNSiRz8_M" -O catkin_ws3.zip && rm -rf /tmp/cookies.txt
@@ -41,18 +54,17 @@ unzip catkin_ws3.zip
 # sudo chown $USER -R catkin_ws3
 cd ~/catkin_ws3
 find ./ -type f -exec sed -i -e "s/mjiang24/$USER/g" {} \;
-pip3 install catkin_pkg
-pip3 install numpy
-pip3 install empy
-pip3 install pathlib
-sudo apt-get install ros-kinetic-geographic-msgs
-sudo apt-get install ros-kinetic-ros-control ros-kinetic-ros-controllers
-sudo apt-get install ros-kinetic-ackermann-msgs
-sudo apt-get install python3-yaml
-sudo apt-get install python3-catkin-pkg-modules
-sudo apt-get install python3-rospkg-modules
+
 
 # Environment setup
 echo "source ~/catkin_ws3/devel/setup.bash" >> ~/.bashrc
+echo "export SVGA_VGPU10=0" >> ~/.bashrc
 source ~/.bashrc
 
+
+echo "------------------------ dependency installation finished -----------------------------"
+perl -plne 'print "set(CMAKE_CXX_FLAGS \"\$\{CMAKE_CXX_FLAGS\} -std=c++14\")" if(/set\(CATKIN_TOPLEVEL TRUE\)/);' /opt/ros/kinetic/share/catkin/cmake/toplevel.cmake > toplevel.cmake
+sudo rm /opt/ros/kinetic/share/catkin/cmake/toplevel.cmake
+sudo mv toplevel.cmake /opt/ros/kinetic/share/catkin/cmake/
+cd ~/catkin_ws3
+catkin_make
