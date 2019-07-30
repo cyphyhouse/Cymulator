4464c23615790b87960ebd8d1d57879be472c67e
Hsieh, Chiao <chsieh16@illinois.edu>
2019-07-30 11:51:11 -0500

    Fix error due to Python positional arguments

--- cymulator/util.py
+++ cymulator/util.py
@@ -59,60 +59,60 @@ def launch(models, loc):
     :param models: a dictionary indicating number of cars and drones
     :return:
     """
-    root = ET.Element(tag='launch')
+    root = ET.Element('launch')
     # include world file
     include = ET.SubElement(
-        parent=root, tag='include',
+        root, 'include',
         attrib={'file': '$(find gazebo_ros)/launch/empty_world.launch'})
     arg = ET.SubElement(
-        parent=include, tag='arg',
+        include, 'arg',
         attrib={'name': 'world_name', 'value': '$(find cyphyhouse)/worlds/cyphyhouse.world'})
 
     # add drones
     for i in range(models['drone']):
         id_str = "drone" + str(i+1)
         group = ET.SubElement(
-            parent=root, tag='group',
+            root, 'group',
             attrib={'ns': id_str})
         include = ET.SubElement(
-            parent=group, tag='include',
+            group, 'include',
             attrib={'file': '$(find hector_quadrotor_gazebo)/launch/spawn_quadrotor.launch'})
         ET.SubElement(
-            parent=include, tag='arg',
+            include, 'arg',
             attrib={'name': 'name', 'value': id_str})
         ET.SubElement(
-            parent=include, tag='arg',
+            include, 'arg',
             attrib={'name': 'model',
                     'value': '$(find hector_quadrotor_description)/urdf/quadrotor_hokuyo_utm30lx.gazebo.xacro'})
         ET.SubElement(
-            parent=include, tag='arg',
+            include, 'arg',
             attrib={'name': 'controllers', 'value': 'controller/attitude controller/velocity controller/position'})
 
         x, y, z = loc['drone'][i]
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'x', 'value': str(x)})
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'y', 'value': str(y)})
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'z', 'value': str(z)})
+        ET.SubElement(include, 'arg', attrib={'name': 'x', 'value': str(x)})
+        ET.SubElement(include, 'arg', attrib={'name': 'y', 'value': str(y)})
+        ET.SubElement(include, 'arg', attrib={'name': 'z', 'value': str(z)})
 
     for i in range(models['car']):
         id_str = "car" + str(i + 1)
-        group = ET.SubElement(parent=root, tag='group', attrib={'ns': id_str})
-        include = ET.SubElement(parent=group, tag='include', attrib={'file': '$(find f1tenth)/launch/car.launch'})
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'name', 'value': id_str})
+        group = ET.SubElement(root, 'group', attrib={'ns': id_str})
+        include = ET.SubElement(group, 'include', attrib={'file': '$(find f1tenth)/launch/car.launch'})
+        ET.SubElement(include, 'arg', attrib={'name': 'name', 'value': id_str})
 
         x, y, z = loc['car'][i]
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'x', 'value': str(x)})
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'y', 'value': str(y)})
-        ET.SubElement(parent=include, tag='arg', attrib={'name': 'z', 'value': str(z)})
+        ET.SubElement(include, 'arg', attrib={'name': 'x', 'value': str(x)})
+        ET.SubElement(include, 'arg', attrib={'name': 'y', 'value': str(y)})
+        ET.SubElement(include, 'arg', attrib={'name': 'z', 'value': str(z)})
 
         include = ET.SubElement(
-            parent=root, tag='include',
+            root, 'include',
             attrib={'file': '$(find f1tenth)/launch/car_control.launch'})
-        ET.SubElement(include, tag='arg', attrib={'name': 'name', 'value': id_str})
+        ET.SubElement(include, 'arg', attrib={'name': 'name', 'value': id_str})
 
         include = ET.SubElement(
-            parent=root, tag='include',
-            attrib={'file': '$(find cyphy_car_mpc)/launch/mpc_controller.launch'})
-        ET.SubElement(include, tag='arg', attrib={'name': 'name', 'value': id_str})
+            root, 'include',
+            attrib={'file': '$(find cyphy_car_mpc)/launch/waypoint_node.launch'})
+        ET.SubElement(include, 'arg', attrib={'name': 'name', 'value': id_str})
 
     tree = ET.ElementTree(root)
     model_path = str(Path.home()) + "/catkin_ws3/src/cyphyhouse/launch/cyphyhouse.launch"
