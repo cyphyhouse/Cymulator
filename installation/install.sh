a1f5300116cfda08737b84829a6a48f864f4efbc
Chiao Hsieh <chsieh16@illinois.edu>
2019-07-30 19:58:01 -0500

    Install ipopt via apt instead of source code

--- installation/install.sh
+++ installation/install.sh
@@ -4,7 +4,6 @@ cd ~
 
 if [ ! "$(rosversion -d)" ]; then
     # Setup your sources.list
-    # TODO Use `add-apt-repository` instead
     sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
     # Set up your keys
@@ -21,15 +20,10 @@ if [ ! "$(rosversion -d)" ]; then
     rosdep update
 fi
 
-# Environment setup
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
 
 # Install Gazebo
 if [ ! -x "$(command -v gazebo)" ]; then
     curl -sSL http://get.gazebosim.org | sh
-    echo "export SVGA_VGPU10=0" >> ~/.bashrc  # For running Gazebo in virtual machine
-    source ~/.bashrc
 fi
 
 # Other ROS packages
@@ -39,6 +33,8 @@ sudo apt install -y \
     ros-kinetic-ros-control ros-kinetic-ros-controllers \
     ros-kinetic-tf2-bullet
 
+# Other System packages
+sudo apt install -y cppad coinor-libipopt-dev  # For MPC controller
 
 # Python Packages
 sudo apt install -y python3 python3-pip
@@ -49,38 +45,20 @@ pip3 install --user \
     pathlib pyyaml
 
 
-#-------------------------------------------------------------------------
-# Download the catkin workspace
-cd ~
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qbtppHGp315hBXq-yoChIv1rNSiRz8_M' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qbtppHGp315hBXq-yoChIv1rNSiRz8_M" -O catkin_ws3.zip && rm -rf /tmp/cookies.txt
-unzip catkin_ws3.zip
-
-# Prepare dependencies for catkin_make
-# sudo chown $USER -R catkin_ws3
-cd ~/catkin_ws3
-# FIXME Rebuild the zip file with the following string replaced
-find ./ -type f -exec sed -i -e "s/mjiang24/$USER/g" {} \;
-
+# TODO Download the catkin workspace
+echo "Please find Chiao to get an updated zip file with catkin workspace for now"
+exit 0
 
 # Environment setup
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 echo "source ~/catkin_ws3/devel/setup.bash" >> ~/.bashrc
-echo "export SVGA_VGPU10=0" >> ~/.bashrc  # TODO Duplication. Why?
+echo "export SVGA_VGPU10=0" >> ~/.bashrc  # For running Gazebo in virtual machine
 source ~/.bashrc
 
 
 echo "------------------------ dependency installation finished -----------------------------"
-#build car mpc
-# TODO Why not apt install coinor-libipopt-dev or ROS ros-kinetic-ifopt
-wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.13.zip -P ~/
-cd ~/
-unzip Ipopt-3.12.13.zip
-rm Ipopt-3.12.13.zip
-cd CyPyHous3/src/Simulator/installation
-sudo bash install_ipopt.sh $HOME/Ipopt-3.12.13/
-sudo apt install cppad
-echo "export LD_LIBRARY_PATH=/usr/local/lib/:${LD_LIBRARY_PATH}" >> ~/.bashrc
-cd ~/catkin_ws3
 
+# Compile all ROS packages
+cd ~/catkin_ws3
 catkin_make --cmake-args -DCMAKE_CXX_STANDARD=14  # Compile with c++14
 
-# TODO Move all `source` commands to one place
