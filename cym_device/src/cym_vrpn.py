ace202bedc58d5dd9c44ab316c723bfd69514318
Hsieh, Chiao <chsieh16@illinois.edu>
2019-08-02 11:13:09 -0500

    Fix some error messages temporarily and add comments

--- cym_device/src/cym_vrpn.py
+++ cym_device/src/cym_vrpn.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ROS node mimicking VRPN server to publish positions of all devices"""
 
-from typing import NamedTuple, Tuple, Optional
+from typing import NamedTuple, Tuple, Optional, List
 
 from gazebo_msgs.srv import GetModelState
 from geometry_msgs.msg import PoseStamped, TwistStamped
@@ -16,6 +16,11 @@ VRPNTopicPublishers = NamedTuple(
 
 
 def get_position(model_name: str) -> Optional[Tuple[PoseStamped, TwistStamped]]:
+    """
+    Get the position of a given model from Gazebo
+    :param model_name: Name of the Gazebo model
+    :return: Position in the types used by VRPN
+    """
     service_name = '/gazebo/get_model_state'
     rospy.wait_for_service(service_name)
     get_model_state = rospy.ServiceProxy(service_name, GetModelState)
@@ -35,15 +40,16 @@ def get_position(model_name: str) -> Optional[Tuple[PoseStamped, TwistStamped]]:
     return pose_stamped, twist_stamped
 
 
-def main(argv):
-    """ Main entry point
+def main(argv: List[str]) -> None:
+    """
+     Main entry point
         This design of putting all pub/sub in main function is intentional
         since we want to avoid pub/sub being instantiated more than once.
+    :param argv:
     """
     # TODO Read from parameter server instead
-    tracker_list = argv[1:4]
+    tracker_list = [arg for arg in argv[1:] if ":" not in arg]
     rospy.init_node('vrpn_client_node')
-    rate = rospy.Rate(10)  # 10hz TODO Pass sleep rate as a parameter?
     # Queue size is 1 since old positions are not needed
     pub = {}
     for tracker_id in tracker_list:
@@ -51,9 +57,12 @@ def main(argv):
             pose=rospy.Publisher("~" + tracker_id + "/pose", PoseStamped, queue_size=1),
             twist=rospy.Publisher("~" + tracker_id + "/twist", TwistStamped, queue_size=1)
         )
+    # TODO Check if Gazebo finished creating all models to avoid the error messages
+    #  "GetModelState: model [model_name] does not exist"
     # XXX Should we wait until all models are created?
-    # Gazebo throws errors because this may try to get model states even before the model is spawned
-    # On the otherhand, if the model is never spawned then waiting can be problematic.
+    #  Gazebo throws errors because this may try to get model states even before the model is spawned
+    #  On the other hand, if the model is never spawned or deleted then waiting can be problematic.
+    rate = rospy.Rate(10)  # 10hz TODO Pass sleep rate as a parameter?
     while not rospy.is_shutdown():
         for tracker_id in tracker_list:
             position = get_position(tracker_id)
@@ -64,7 +73,11 @@ def main(argv):
                 pose, twist = position
                 pub[tracker_id].pose.publish(pose)
                 pub[tracker_id].twist.publish(twist)
-        rate.sleep()
+        try:
+            rate.sleep()
+        except rospy.exceptions.ROSInterruptException:
+            rospy.loginfo("Shutting down CymVRPN")
+            break
 
 
 if __name__ == "__main__":
