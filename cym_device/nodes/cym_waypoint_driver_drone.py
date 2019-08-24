ac951fcecc64642d6033dc6a28e595625f401cd6
Hsieh, Chiao <chsieh16@illinois.edu>
2019-08-23 21:24:33 -0500

    Rewrite drone driver with built-in pose based controller in hector quadrotor

--- cym_device/nodes/cym_waypoint_driver_drone.py
+++ cym_device/nodes/cym_waypoint_driver_drone.py
@@ -5,26 +5,30 @@
 from copy import deepcopy
 from math import copysign
 from queue import Queue
+from threading import RLock
 from typing import Tuple
 
 from geometry_msgs.msg import PoseStamped, Twist, TwistStamped
 from hector_uav_msgs.srv import EnableMotors
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Bool, String
 
 
 class __DroneStates:
+
     def __init__(self,
                  init_pose: PoseStamped,
-                 init_twist: TwistStamped,
-                 init_waypoint: PoseStamped):
+                 init_twist: TwistStamped):
         assert init_pose
         assert init_twist
-        assert init_waypoint
-        self._curr_waypoint = init_waypoint
-        self._waypoints = Queue()  # Thread safe Queue is required because pub and sub can be in different threads
+
+        self._pose_lock = RLock()
         self._pose = init_pose
+        self._twist_lock = RLock()
         self._twist = init_twist
+
+        self._curr_waypoint = init_pose
+        self._waypoints = Queue()  # Thread safe Queue is required because pub and sub can be in different threads
 
     def store_waypoint(self, msg: PoseStamped) -> None:
         """
@@ -40,7 +44,8 @@ class __DroneStates:
         :param msg: PoseStamped type msg
         :return: Nothing
         """
-        self._pose = deepcopy(msg)  # In case `msg` is overwritten
+        with self._pose_lock:
+            self._pose = deepcopy(msg)  # In case `msg` is overwritten
 
     def store_twist(self, msg: PoseStamped) -> None:
         """
@@ -48,11 +53,15 @@ class __DroneStates:
         :param msg: TwistStamped type msg
         :return: Nothing
         """
-        self._twist = deepcopy(msg)  # In case `msg` is overwritten
+        with self._twist_lock:
+            self._twist = deepcopy(msg)  # In case `msg` is overwritten
 
-    def is_reached(self) -> bool:
+    def has_reached(self) -> bool:
+        assert self._curr_waypoint
         goal = self._curr_waypoint.pose.position
-        curr = self._pose.pose.position
+
+        with self._pose_lock:
+            curr = self._pose.pose.position
 
         # FIXME pass a function to decide whether the waypoint is reached?
         dist_2 = (goal.x - curr.x) ** 2 + \
@@ -60,28 +69,16 @@ class __DroneStates:
                  (goal.z - curr.z) ** 2
         return dist_2 < (0.25 ** 2)  # sqrt(dist_2) < 0.25
 
-    def set_next_waypoint(self):
-        if not self._waypoints.empty():
-            self._curr_waypoint = self._waypoints.get()
-        # else: _curr_waypoint remains so is_reached should be true
+    def set_curr_waypoint(self):
+        assert not self._waypoints.empty()
+        self._curr_waypoint = self._waypoints.get()
+        print("Set new waypoint to", self._curr_waypoint)
 
-    def cmd_vel(self) -> Twist:
-        if self.is_reached():
-            return Twist()  # Stop movement
-        # else:
-        goal = self._curr_waypoint.pose.position
-        curr = self._pose.pose.position
-
-        # FIXME pass a function to calculate velocity?
-        def cal_vel(diff: float) -> float:
-            magnitude = max(abs(diff), 0.5)
-            return copysign(magnitude, diff)
-
-        move_cmd = Twist()
-        move_cmd.linear.x = cal_vel(goal.x - curr.x)
-        move_cmd.linear.y = cal_vel(goal.y - curr.y)
-        move_cmd.linear.z = cal_vel(goal.z - curr.z)
-        return move_cmd
+    def cmd_pose(self) -> PoseStamped:
+        assert self._curr_waypoint
+        pose_cmd = deepcopy(self._curr_waypoint)
+        pose_cmd.header.frame_id = "world"
+        return pose_cmd
 
 
 def main(argv) -> None:
@@ -97,7 +94,9 @@ def main(argv) -> None:
     # Register publishers first
     pub_reached = rospy.Publisher("~reached", String, queue_size=1)  # FIXME how to decide queue_size
     # For driving the simulated drone
-    pub_cmd_vel = rospy.Publisher("cmd_vel", Twist, queue_size=10)  # FIXME how to decide queue_size
+    # FIXME Should use actionlib server provided by hector quadrotor instead
+    pub_cmd_pose = rospy.Publisher("command/pose", PoseStamped, queue_size=10)  # FIXME how to decide queue_size
+    pub_estop = rospy.Publisher("estop", Bool, queue_size=1)
 
     # Register subscribers
     # Wait for positioning system to start
@@ -106,9 +105,8 @@ def main(argv) -> None:
     waypoint_topc_name = "~waypoint"
     init_pose = rospy.wait_for_message(pose_topic_name, PoseStamped)
     init_twist = rospy.wait_for_message(twist_topic_name, TwistStamped)
-    init_waypoint = rospy.wait_for_message(waypoint_topc_name, PoseStamped)
 
-    ds = __DroneStates(init_pose, init_twist, init_waypoint)
+    ds = __DroneStates(init_pose, init_twist)
     # For positioning
     _ = rospy.Subscriber(pose_topic_name, PoseStamped, ds.store_pose)
     _ = rospy.Subscriber(twist_topic_name, TwistStamped, ds.store_twist)
@@ -129,22 +127,31 @@ def main(argv) -> None:
 
     def shutdown() -> None:  # TODO Better place for this code
         """Stop the drone when this ROS node shuts down"""
-        pub_cmd_vel.publish(Twist())  # Default Twist will stop the drone
+        pub_estop.publish(Bool(True))  # Emergency stop
         # TODO Safe landing
         rospy.loginfo("Stop the drone")
 
     rospy.on_shutdown(shutdown)
 
-    rate = rospy.Rate(10)  # 10 hz TODO Pass sleep rate as a parameter?
+    # TODO Wait for hector quadrotor controllers to spawn
+    rospy.sleep(1)
+
+    rate = rospy.Rate(50)  # TODO Pass sleep rate as a parameter?
+    is_driving = False
     while not rospy.is_shutdown():
-        # Simple controller code for drones # TODO Need better controller
-
-        pub_reached.publish(str(ds.is_reached()))
-        pub_cmd_vel.publish(ds.cmd_vel())
-        if ds.is_reached():
-            ds.set_next_waypoint()
-
         rate.sleep()
+        # Simple controller code for drones # TODO Need better controller
+        if not is_driving:  # IDLE
+            if not ds._waypoints.empty():  # FIXME accessing protected member
+                ds.set_curr_waypoint()
+                is_driving = True
+            # else: is_driving = False  # Keep idling
+        else:  # DRIVING
+            pub_cmd_pose.publish(ds.cmd_pose())
+            if ds.has_reached():
+                pub_reached.publish(str(True))
+                is_driving = False
+            # else: is_driving = True  # Keep driving
 
 
 if __name__ == "__main__":
