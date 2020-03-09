518cf2a3c486f52cd20d05637c12ddcfa9d5bf25
Hsieh, Chiao <chsieh16@illinois.edu>
2020-03-08 22:36:56 -0500

    Add conversion for Marker messages

---
+++ cym_marker/include/ros_ign_bridge/convert_builtin_interfaces.hpp
@@ -0,0 +1,228 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ROS_IGN_BRIDGE__CONVERT_BUILTIN_INTERFACES_HPP_
+#define ROS_IGN_BRIDGE__CONVERT_BUILTIN_INTERFACES_HPP_
+
+// include ROS builtin messages
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <rosgraph_msgs/Clock.h>
+#include <geometry_msgs/Transform.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/ColorRGBA.h>
+#include <std_msgs/Empty.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/String.h>
+#include <visualization_msgs/Marker.h>
+
+// include Ignition builtin messages
+#include <ignition/msgs.hh>
+
+#include "ros_ign_bridge/convert_decl.hpp"
+
+namespace ros_ign_bridge
+{
+
+// std_msgs
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::Bool & ros_msg,
+  ignition::msgs::Boolean & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Boolean & ign_msg,
+  std_msgs::Bool & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::ColorRGBA & ros_msg,
+  ignition::msgs::Color & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Color & ign_msg,
+  std_msgs::ColorRGBA & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::Empty & ros_msg,
+  ignition::msgs::Empty & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Empty & ign_msg,
+  std_msgs::Empty & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::Float32 & ros_msg,
+  ignition::msgs::Float & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Float & ign_msg,
+  std_msgs::Float32 & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::Header & ros_msg,
+  ignition::msgs::Header & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Header & ign_msg,
+  std_msgs::Header & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const std_msgs::String & ros_msg,
+  ignition::msgs::StringMsg & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::StringMsg & ign_msg,
+  std_msgs::String & ros_msg);
+
+// rosgraph_msgs
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Clock & ign_msg,
+  rosgraph_msgs::Clock & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const rosgraph_msgs::Clock & ros_msg,
+  ignition::msgs::Clock & ign_msg);
+
+// geometry_msgs
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::Quaternion & ros_msg,
+  ignition::msgs::Quaternion & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Quaternion & ign_msg,
+  geometry_msgs::Quaternion & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::Vector3 & ros_msg,
+  ignition::msgs::Vector3d & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Vector3d & ign_msg,
+  geometry_msgs::Vector3 & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::Point & ros_msg,
+  ignition::msgs::Vector3d & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Vector3d & ign_msg,
+  geometry_msgs::Point & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::Pose & ros_msg,
+  ignition::msgs::Pose & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Pose & ign_msg,
+  geometry_msgs::Pose & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::PoseStamped & ros_msg,
+  ignition::msgs::Pose & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Pose & ign_msg,
+  geometry_msgs::PoseStamped & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::Transform & ros_msg,
+  ignition::msgs::Pose & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Pose & ign_msg,
+  geometry_msgs::Transform & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const geometry_msgs::TransformStamped & ros_msg,
+  ignition::msgs::Pose & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Pose & ign_msg,
+  geometry_msgs::TransformStamped & ros_msg);
+
+template<>
+void
+convert_ros_to_ign(
+  const visualization_msgs::Marker & ros_msg,
+  ignition::msgs::Marker & ign_msg);
+
+template<>
+void
+convert_ign_to_ros(
+  const ignition::msgs::Marker & ign_msg,
+  visualization_msgs::Marker & ros_msg);
+
+}  // namespace ros_ign_bridge
+
+#endif  // ROS_IGN_BRIDGE__CONVERT_BUILTIN_INTERFACES_HPP_
