693829f0cc443953afe172d4696a50908a03281d
Hsieh, Chiao <chsieh16@illinois.edu>
2021-02-26 10:46:21 -0600

    Add optional init_yaw in scene

--- cym_gazebo/src/cym_gazebo/launcher.py
+++ cym_gazebo/src/cym_gazebo/launcher.py
@@ -3,15 +3,16 @@ APIs creating a ROSLaunch XML tree that after starting it will launch Gazebo
  with desired number of cars, drones, and specified obstacles
 """
 
-from typing import List, NamedTuple
+from typing import Any, Dict, NamedTuple, Sequence
 import xml.etree.ElementTree as ET
 
 from geometry_msgs.msg import Point
+import rospy
 
 
 DeviceInitInfo = NamedTuple(
     "DeviceInitInfo",
-    [("bot_name", str), ("bot_type", str), ("position", Point)]
+    [("bot_name", str), ("bot_type", str), ("position", Point), ("yaw", float)]
 )
 
 
@@ -23,7 +24,7 @@ def gen_launch_element_tree(cfg) -> ET.ElementTree:
     """
     if isinstance(cfg, list):
         world_name = "irl_arena.world"
-        cfg_devices = cfg
+        cfg_devices = cfg  # type: Sequence[Dict[str, Any]]
     elif isinstance(cfg, dict):
         world_name = cfg['world_name']
         cfg_devices = cfg['devices']
@@ -31,7 +32,11 @@ def gen_launch_element_tree(cfg) -> ET.ElementTree:
         raise ValueError("Unexpected value in YAML file")
 
     device_list = [
-        DeviceInitInfo(device["bot_name"], device["bot_type"], Point(*device["init_pos"]))
+        DeviceInitInfo(
+            bot_name=device["bot_name"],
+            bot_type=device["bot_type"],
+            position=Point(*device["init_pos"]),
+            yaw=device.get("init_yaw", 0.0))
         for device in cfg_devices
     ]
 
@@ -75,5 +80,9 @@ def gen_launch_element_tree(cfg) -> ET.ElementTree:
         ET.SubElement(include, 'arg', attrib={'name': 'x', 'value': str(device.position.x)})
         ET.SubElement(include, 'arg', attrib={'name': 'y', 'value': str(device.position.y)})
         ET.SubElement(include, 'arg', attrib={'name': 'z', 'value': str(device.position.z)})
+
+        if device.yaw != 0.0 and cym_device_model == "hector_quadrotor":
+            rospy.logwarn("Warning: Initial yaw heading %f of %s will be ignored by Hector Quadrotor model." % (device.yaw, device.bot_name))
+        ET.SubElement(include, 'arg', attrib={'name': 'yaw', 'value': str(device.yaw)})
 
     return ET.ElementTree(root)
